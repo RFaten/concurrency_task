31f6327b78135f67bd653c5e90625fba38067e2f
FatenRostom-at-733083339469 <faten@gmail.com>
2024-12-06 18:04:50 +0100

    Task1- Use ConcurrentHashMap

--- src/main/java/org/example/task1/Main.java
+++ src/main/java/org/example/task1/Main.java
@@ -1,15 +1,39 @@
 package org.example.task1;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
+    enum caseToTest {HashMap, ConcurrentHashMap, SynchronizedMap, ThreadSafeMap};
     public static void main(String[] args) {
-        Map<Integer, Integer> map = new HashMap<>();
+
+        Map<Integer, Integer> map = testTask1(caseToTest.ConcurrentHashMap);
         Thread writerThread = new Thread(new WriterRunnable(map));
         Thread readerThread = new Thread(new ReaderRunnable(map));
         writerThread.start();
         readerThread.start();
 
+    }
+
+    private static Map<Integer, Integer> testTask1(caseToTest caseToTest) {
+        switch (caseToTest) {
+            case HashMap -> {
+                return new HashMap<>();
+            }
+            case ConcurrentHashMap -> {
+                return new ConcurrentHashMap<>();
+            }
+            case SynchronizedMap -> {
+                return Collections.synchronizedMap(new HashMap<>());
+            }
+            case ThreadSafeMap -> {
+                return new ThreadSafeMap();
+            }
+            default -> {
+                return null;
+            }
+        }
     }
 }
---
+++ src/main/java/org/example/task1/ThreadSafeMap.java
@@ -0,0 +1,6 @@
+package org.example.task1;
+
+import java.util.HashMap;
+
+public class ThreadSafeMap extends HashMap<Integer, Integer> {
+}
